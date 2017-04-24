1964498c718954ffc7f6059f23b75dbb430c415b
Alex Ooi <alexooi1990@gmail.com>
2017-04-24 19:30:57 +0800

    Added Project start, Login Prototype and updated discussion

---
+++ Project/NYPITP312/NYPITP312Tests/NYPITP312Tests.swift
@@ -0,0 +1,36 @@
+//
+//  NYPITP312Tests.swift
+//  NYPITP312Tests
+//
+//  Created by Alex Ooi on 24/4/17.
+//  Copyright © 2017 NYP. All rights reserved.
+//
+
+import XCTest
+@testable import NYPITP312
+
+class NYPITP312Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
