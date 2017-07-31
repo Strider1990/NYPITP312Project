0d1a035a03d7e5aa8a384abe9a95fc9eeddef773
xlepotato <yucia181@gmail.com>
2017-08-01 07:43:44 +0800

    Added ISBN entity, data manager & related VC

---
+++ Project/NYPITP312/NYPITP312/ISBN.swift
@@ -0,0 +1,31 @@
+//
+//  ISBN.swift
+//  NYPITP312
+//
+//  Created by Ng Wan Ying on 1/8/17.
+//  Copyright © 2017 NYP. All rights reserved.
+//
+//
+//  ISBN.swift
+//  FairpriceShareTextbook
+//
+//  Created by KIM FOONG CHOW on 27/5/17.
+//  Copyright © 2017 NYP. All rights reserved.
+//
+
+import UIKit
+
+class ISBN: NSObject {
+    
+    var id : String?
+    var name: String?
+    var publisher : String?
+    var author : String?
+    var edition : String?
+    var cateid : [String] = []
+    
+    
+    
+    
+    
+}
